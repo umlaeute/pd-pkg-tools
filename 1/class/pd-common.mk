305bf3f35867db81c2d2adc51dcedc3ad25b5a07
IOhannes m zmölnig <zmoelnig@iem.at>
2010-12-03 11:38:33 +0100

    split into flavoured cdbs-snippets

--- 1/class/pd-common.mk
+++ 1/class/pd-common.mk
@@ -26,9 +26,6 @@ endif
 ifndef _cdbs_class_pd_common
 _cdbs_class_pd_common := 1
 
-CDBS_BUILD_DEPENDS_class_pd_common ?= pd
-CDBS_BUILD_DEPENDS += , $(CDBS_BUILD_DEPENDS_class_pd_common)
-
 include $(_cdbs_class_path)/langcore.mk$(_cdbs_makefile_suffix)
 include $(_cdbs_rules_path)/buildcore.mk$(_cdbs_makefile_suffix)
 
