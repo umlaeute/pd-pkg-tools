46d8787d4f9af40c143d3ad8a39afd970993c664
IOhannes m zmölnig <zmoelnig@iem.at>
2011-01-04 14:16:58 +0100

    fix FSF address
    use website rather than postal address

--- 1/class/pd-common.mk
+++ 1/class/pd-common.mk
@@ -1,6 +1,5 @@
 # -*- mode: makefile; coding: utf-8 -*-
 # Copyright © 2010 IOhannes m zmölnig <zmoelnig@iem.at>
-
 #
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License as
@@ -13,9 +12,7 @@
 # General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with this program; if not, write to the Free Software
-# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
-# 02111-1307 USA.
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 ifndef _cdbs_bootstrap
 _cdbs_scripts_path ?= /usr/lib/cdbs
